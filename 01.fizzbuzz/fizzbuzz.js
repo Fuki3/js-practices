5161f70a1f89465a428f71211c4094982b46fd5c
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2024-08-16 12:31:55 +0900

    Fizzbuzz出力時の条件の表現方法の変更

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 for (let number = 1; number <= 20; number++) {
-  if (number % 15 === 0) {
+  if (number % 3 === 0 && number % 5 === 0) {
     console.log("Fizzbuzz");
   } else if (number % 3 === 0) {
     console.log("Fizz");
