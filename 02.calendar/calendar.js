078b230deaf029d52e947f1a64aa2d7358c9a919
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2025-03-09 00:19:52 +0900

    変数名firstDayをdayに変更

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -7,28 +7,24 @@ const argv = minimist(process.argv.slice(2));
 const now = new Date();
 const year = argv.y ?? now.getFullYear();
 const month = argv.m ?? now.getMonth() + 1;
-const firstDay = new Date(year, month - 1, 1);
+const day = new Date(year, month - 1, 1);
 const lastDay = new Date(year, month, 0);
 
 console.log(`      ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
 
-for (
-  let numberOfBlank = 0;
-  numberOfBlank < firstDay.getDay();
-  numberOfBlank++
-) {
+for (let numberOfBlank = 0; numberOfBlank < day.getDay(); numberOfBlank++) {
   process.stdout.write("   ");
 }
-for (let eachDay = firstDay.getDate(); eachDay < lastDay.getDate(); eachDay++) {
+for (let eachDay = day.getDate(); eachDay < lastDay.getDate(); eachDay++) {
   if (String(eachDay).length === 1) {
     process.stdout.write(" ");
   }
-  firstDay.setDate(eachDay);
-  if (firstDay.getDay() === 6) {
-    process.stdout.write(firstDay.getDate() + "\n");
+  day.setDate(eachDay);
+  if (day.getDay() === 6) {
+    process.stdout.write(day.getDate() + "\n");
   } else {
-    process.stdout.write(firstDay.getDate() + " ");
+    process.stdout.write(day.getDate() + " ");
   }
 }
 process.stdout.write(lastDay.getDate() + "\n" + "\n");
