a1f95d49419221d5cf4cfa9d8519d00d9ac54d2d
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2025-03-09 00:03:50 +0900

    変数名の修正

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -4,30 +4,34 @@ import minimist from "minimist";
 
 const argv = minimist(process.argv.slice(2));
 
-const now_date = new Date();
-const output_year = argv.y ?? now_date.getFullYear();
-const output_month = argv.m ?? now_date.getMonth() + 1;
-const first_day = new Date(output_year, output_month - 1, 1);
-const last_day = new Date(output_year, output_month, 0);
-const week = first_day.getDay();
+const now = new Date();
+const year = argv.y ?? now.getFullYear();
+const month = argv.m ?? now.getMonth() + 1;
+const firstDay = new Date(year, month - 1, 1);
+const lastDay = new Date(year, month, 0);
+const week = firstDay.getDay();
 
-console.log("      " + output_month + "月" + " " + output_year);
+console.log("      " + month + "月" + " " + year);
 console.log("日 月 火 水 木 金 土");
 
-let week_count = week;
-for (let blank = 0; blank < week_count; blank++) {
+let weekCount = week;
+for (let numberOfBlank = 0; numberOfBlank < weekCount; numberOfBlank++) {
   process.stdout.write("   ");
 }
-for (let number = first_day.getDate(); number <= last_day.getDate(); number++) {
-  if (String(number).length === 1) {
+for (
+  let eachDay = firstDay.getDate();
+  eachDay <= lastDay.getDate();
+  eachDay++
+) {
+  if (String(eachDay).length === 1) {
     process.stdout.write(" ");
   }
-  process.stdout.write(number + " ");
-  if (week_count === 6) {
+  process.stdout.write(eachDay + " ");
+  if (weekCount === 6) {
     process.stdout.write("\n");
-    week_count = 0;
+    weekCount = 0;
   } else {
-    week_count++;
+    weekCount++;
   }
 }
 process.stdout.write("\n" + "\n");
