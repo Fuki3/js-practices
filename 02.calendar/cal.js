8faba36121f390e534bf892aa856c40b5f5dca18
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2025-03-24 23:32:01 +0900

    変数名dayをfirstDayに修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -7,23 +7,25 @@ const argv = minimist(process.argv.slice(2));
 const date = new Date();
 const year = argv.y ?? date.getFullYear();
 const month = argv.m ?? date.getMonth() + 1;
-const day = new Date(year, month - 1, 1);
+const firstDay = new Date(year, month - 1, 1);
 const lastDay = new Date(year, month, 0);
 
 console.log(`      ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
 
-process.stdout.write(" ".repeat(day.getDay() * 3));
+process.stdout.write(" ".repeat(firstDay.getDay() * 3));
 
-for (let date = day; date < lastDay; date.setDate(date.getDate() + 1)) {
+for (let date = firstDay; date <= lastDay; date.setDate(date.getDate() + 1)) {
   if (String(date.getDate()).length === 1) {
     process.stdout.write(" ");
   }
   process.stdout.write(String(date.getDate()));
-  if (date.getDay() === 6) {
-    process.stdout.write("\n");
-  } else {
-    process.stdout.write(" ");
+  if (date !== lastDay) {
+    if (date.getDay() === 6) {
+      process.stdout.write("\n");
+    } else {
+      process.stdout.write(" ");
+    }
   }
 }
-process.stdout.write(`${lastDay.getDate()}\n\n`);
+process.stdout.write("\n\n");
