37825f6e95fec0d720418065b11e03e046688d16
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2025-04-05 23:16:59 +0900

    if文を三項演算子に修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -18,11 +18,7 @@ process.stdout.write(" ".repeat(firstDay.getDay() * 3));
 for (let date = firstDay; date <= lastDay; ) {
   process.stdout.write(date.getDate().toString().padStart(2, " "));
   if (date !== lastDay) {
-    if (date.getDay() === 6) {
-      process.stdout.write("\n");
-    } else {
-      process.stdout.write(" ");
-    }
+    process.stdout.write(date.getDay() === 6 ? "\n" : " ");
   }
   date = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1);
 }
