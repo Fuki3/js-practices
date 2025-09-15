289c8b52e4696065523f6a08e5aea10fafe7e152
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2025-09-16 00:44:06 +0900

    コールバックの形式でnode-sqlite3 の関数を使う（エラーあり）

---
+++ 03.asynchronous/callback_err.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+import sqlite3 from "sqlite3";
+
+const db = new sqlite3.Database(":memory:");
+
+db.run(
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)",
+  () => {
+    db.run("INSERT INTO books(title) VALUES(?)", [], (err) => {
+      console.error(`Get Error: ${err.message}`);
+      db.get("SELECT * FROM memo", (err) => {
+        console.error(`Get Error: ${err.message}`);
+        db.run("DROP TABLE books");
+        db.close();
+      });
+    });
+  },
+);
