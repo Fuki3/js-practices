7f3869d941f4061403a6c520b155e15322f38c75
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2025-09-17 00:02:43 +0900

    async, awaitの形式でnode-sqlite3 の関数を使う（エラーあり）

---
+++ 03.asynchronous/async_await_err.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+import { run, get, close } from "./utils.js";
+
+async function LearnAsynchronous() {
+  try {
+    await run(
+      "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)",
+    );
+    await run("INSERT INTO books(title) VALUES(?)");
+  } catch (err) {
+    console.error(`Get Error: ${err.message}`);
+  }
+  try {
+    await get("SELECT * FROM memo");
+  } catch (err) {
+    console.error(`Get Error: ${err.message}`);
+    await run("DROP TABLE books");
+  } finally {
+    close();
+  }
+}
+
+LearnAsynchronous();
