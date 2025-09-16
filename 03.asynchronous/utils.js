fe6ee9d9abf0af82f2839f7ccd7e20a7b239b834
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2025-09-16 23:59:02 +0900

    Promiseベースの関数を作成

---
+++ 03.asynchronous/utils.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+import sqlite3 from "sqlite3";
+
+const db = new sqlite3.Database(":memory:");
+
+function run(sql, params) {
+  return new Promise((resolve, reject) => {
+    db.run(sql, [params], function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ lastID: this.lastID });
+      }
+    });
+  });
+}
+
+function get(sql, params) {
+  return new Promise((resolve, reject) => {
+    db.get(sql, [params], function (err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+function close() {
+  db.close();
+}
+
+export { run, get, close };
