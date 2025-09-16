af4a3bff545df9e862f564d9701b3c52e557649e
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2025-09-17 00:01:23 +0900

    Promiseの形式でnode-sqlite3 の関数を使う（エラーあり）

---
+++ 03.asynchronous/promise_err.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import { run, get, close } from "./utils.js";
+
+run(
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)",
+)
+  .then(() => {
+    return run("INSERT INTO books(title) VALUES(?)");
+  })
+  .catch((err) => {
+    console.error(`Get Error: ${err.message}`);
+    return get("SELECT * FROM memo");
+  })
+  .catch((err) => {
+    console.error(`Get Error: ${err.message}`);
+    return run("DROP TABLE books");
+  })
+  .then(() => {
+    return close();
+  });
