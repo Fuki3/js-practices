5f1e2ca539cbfaf0931b91a055e7896af92ac303
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2025-09-20 23:46:22 +0900

    Top-level awaitに修正

--- 03.asynchronous/async_await.js
+++ 03.asynchronous/async_await.js
@@ -5,24 +5,20 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-async function LearnAsynchronous() {
-  try {
-    await run(
-      db,
-      "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)",
-    );
-    const newRecord = await run(
-      db,
-      "INSERT INTO books(title) VALUES(?)",
-      "初めてのJavaScript",
-    );
-    console.log(newRecord.lastID);
-    const getRecord = await get(db, "SELECT * FROM books");
-    console.log(getRecord);
-    await run(db, "DROP TABLE books");
-  } finally {
-    close(db);
-  }
+try {
+  await run(
+    db,
+    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)",
+  );
+  const newRecord = await run(
+    db,
+    "INSERT INTO books(title) VALUES(?)",
+    "初めてのJavaScript",
+  );
+  console.log(newRecord.lastID);
+  const getRecord = await get(db, "SELECT * FROM books");
+  console.log(getRecord);
+  await run(db, "DROP TABLE books");
+} finally {
+  close(db);
 }
-
-LearnAsynchronous();
--- 03.asynchronous/async_await_error.js
+++ 03.asynchronous/async_await_error.js
@@ -5,24 +5,20 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-async function LearnAsynchronous() {
-  try {
-    await run(
-      db,
-      "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)",
-    );
-    await run(db, "INSERT INTO books(title) VALUES(?)", null);
-  } catch (error) {
-    console.error(`Get Error: ${error.message}`);
-  }
-  try {
-    await get(db, "SELECT * FROM memo");
-  } catch (error) {
-    console.error(`Get Error: ${error.message}`);
-    await run(db, "DROP TABLE books");
-  } finally {
-    close(db);
-  }
+try {
+  await run(
+    db,
+    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)",
+  );
+  await run(db, "INSERT INTO books(title) VALUES(?)", null);
+} catch (error) {
+  console.error(`Get Error: ${error.message}`);
 }
-
-LearnAsynchronous();
+try {
+  await get(db, "SELECT * FROM memo");
+} catch (error) {
+  console.error(`Get Error: ${error.message}`);
+  await run(db, "DROP TABLE books");
+} finally {
+  close(db);
+}
