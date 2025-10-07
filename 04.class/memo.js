39b14ea8245a9aa326d519565a4f11a3fb1fb186
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2025-10-07 23:09:40 +0900

    -lオプションの追加

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -2,20 +2,51 @@
 
 import fs from "fs/promises";
 import readline from "readline";
+import path from "path";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const argv = process.argv.slice(2);
 
-const lines = [];
+if (argv[0] === "-l") {
+  async function readFirstLine(filePath) {
+    try {
+      const content = await fs.readFile(filePath, "utf8");
+      const firstLine = content.split("\n")[0];
+      return firstLine;
+    } catch (err) {
+      console.error(`Error: ${filePath}`, err);
+      return null;
+    }
+  }
 
-rl.on("line", (input) => {
-  if (input === "") return rl.close();
-  lines.push(input);
-});
+  async function readAllFilesFirstLine(directoryPath) {
+    try {
+      const files = await fs.readdir(directoryPath);
+      for (const file of files) {
+        const Path = path.join(directoryPath, file);
+        const line = await readFirstLine(Path);
+        console.log(line);
+      }
+    } catch (err) {
+      console.error("Error:", err);
+    }
+  }
 
-rl.on("close", async () => {
-  await fs.mkdir("memos", { recursive: true });
-  await fs.writeFile(`./memos/${lines[0]}.txt`, lines.join("\n"));
-});
+  readAllFilesFirstLine("./memos");
+} else {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const lines = [];
+
+  rl.on("line", (input) => {
+    if (input === "") return rl.close();
+    lines.push(input);
+  });
+
+  rl.on("close", async () => {
+    await fs.mkdir("memos", { recursive: true });
+    await fs.writeFile(`./memos/${lines[0]}.txt`, lines.join("\n"));
+  });
+}
