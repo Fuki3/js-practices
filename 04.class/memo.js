89632f7e1af29703045c5526aa39a9855c9967df
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2025-10-09 23:05:03 +0900

    -rオプションの追加

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -3,8 +3,10 @@
 import fs from "fs/promises";
 import readline from "readline";
 import path from "path";
+import enquirer from "enquirer";
 
 const argv = process.argv.slice(2);
+const { Select } = enquirer;
 
 if (argv[0] === "-l") {
   async function readFirstLine(filePath) {
@@ -32,6 +34,52 @@ if (argv[0] === "-l") {
   }
 
   readAllFilesFirstLine("./memos");
+} else if (argv[0] === "-r") {
+  async function readFirstLine(filePath) {
+    try {
+      const content = await fs.readFile(filePath, "utf8");
+      const firstLine = content.split("\n")[0];
+      return firstLine;
+    } catch (err) {
+      console.error(`Error: ${filePath}`, err);
+      return null;
+    }
+  }
+
+  async function readAllFilesFirstLine(directoryPath) {
+    const lines = [];
+    try {
+      const files = await fs.readdir(directoryPath);
+      for (const file of files) {
+        const Path = path.join(directoryPath, file);
+        const line = await readFirstLine(Path);
+        lines.push(line);
+      }
+      return lines;
+    } catch (err) {
+      console.error("Error:", err);
+      return [];
+    }
+  }
+
+  const lines = await readAllFilesFirstLine("./memos");
+
+  const prompt = new Select({
+    message: "Choose a note you want to see:",
+    choices: lines,
+  });
+
+  const answer = await prompt.run();
+
+  async function showList(filePath) {
+    try {
+      const content = await fs.readFile(filePath, "utf8");
+      console.log(content);
+    } catch (err) {
+      console.error("Error:", err.message);
+    }
+  }
+  showList(`./memos/${answer}.txt`);
 } else {
   const rl = readline.createInterface({
     input: process.stdin,
