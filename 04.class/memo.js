f48193c58449791bade154fff599931f551333a1
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2025-11-08 00:20:10 +0900

    クラス構文を使う形に変更

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -5,142 +5,145 @@ import readline from "readline";
 import path from "path";
 import enquirer from "enquirer";
 
-const argv = process.argv.slice(2);
+const option = process.argv.slice(2);
 const { Select } = enquirer;
 
-if (argv[0] === "-l") {
-  async function readFirstLine(filePath) {
-    try {
-      const content = await fs.readFile(filePath, "utf8");
-      const firstLine = content.split("\n")[0];
-      return firstLine;
-    } catch (err) {
-      console.error(`Error: ${filePath}`, err);
-      return null;
-    }
+class MemoApp {
+  constructor(option) {
+    this.option = option;
+    this.lines = [];
   }
 
-  async function readAllFilesFirstLine(directoryPath) {
-    try {
-      const files = await fs.readdir(directoryPath);
-      for (const file of files) {
-        const Path = path.join(directoryPath, file);
-        const line = await readFirstLine(Path);
-        console.log(line);
-      }
-    } catch (err) {
-      console.error("Error:", err);
-    }
+  async input() {
+    return new Promise((resolve) => {
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+
+      rl.on("line", (input) => {
+        if (input === "") {
+          rl.close();
+        } else {
+          this.lines.push(input);
+        }
+      });
+
+      rl.on("close", () => {
+        resolve(this.lines);
+      });
+    });
   }
 
-  readAllFilesFirstLine("./memos");
-} else if (argv[0] === "-r") {
-  async function readFirstLine(filePath) {
-    try {
-      const content = await fs.readFile(filePath, "utf8");
-      const firstLine = content.split("\n")[0];
-      return firstLine;
-    } catch (err) {
-      console.error(`Error: ${filePath}`, err);
-      return null;
-    }
+  async choose(lines, message) {
+    const prompt = new Select({
+      message: message,
+      choices: lines,
+    });
+
+    const answer = await prompt.run();
+    return answer;
   }
 
-  async function readAllFilesFirstLine(directoryPath) {
-    const lines = [];
-    try {
-      const files = await fs.readdir(directoryPath);
-      for (const file of files) {
-        const Path = path.join(directoryPath, file);
-        const line = await readFirstLine(Path);
-        lines.push(line);
-      }
-      return lines;
-    } catch (err) {
-      console.error("Error:", err);
-      return [];
+  async output() {
+    const memo = new Memo();
+    if (this.option[0] === "-l") {
+      memo.print();
+    } else if (this.option[0] === "-r") {
+      memo.printAll();
+    } else if (this.option[0] === "-d") {
+      memo.delete();
+    } else {
+      memo.add();
     }
   }
-
-  const lines = await readAllFilesFirstLine("./memos");
-
-  const prompt = new Select({
-    message: "Choose a note you want to see:",
-    choices: lines,
-  });
-
-  const answer = await prompt.run();
-
-  async function showList(filePath) {
-    try {
-      const content = await fs.readFile(filePath, "utf8");
-      console.log(content);
-    } catch (err) {
-      console.error("Error:", err.message);
-    }
-  }
-  showList(`./memos/${answer}.txt`);
-} else if (argv[0] === "-d") {
-  async function readFirstLine(filePath) {
-    try {
-      const content = await fs.readFile(filePath, "utf8");
-      const firstLine = content.split("\n")[0];
-      return firstLine;
-    } catch (err) {
-      console.error(`Error: ${filePath}`, err);
-      return null;
-    }
-  }
-
-  async function readAllFilesFirstLine(directoryPath) {
-    const lines = [];
-    try {
-      const files = await fs.readdir(directoryPath);
-      for (const file of files) {
-        const Path = path.join(directoryPath, file);
-        const line = await readFirstLine(Path);
-        lines.push(line);
-      }
-      return lines;
-    } catch (err) {
-      console.error("Error:", err);
-      return [];
-    }
-  }
-
-  const lines = await readAllFilesFirstLine("./memos");
-
-  const prompt = new Select({
-    message: "Choose a memo you want to delete:",
-    choices: lines,
-  });
-
-  const answer = await prompt.run();
-
-  async function deleteFile(filePath) {
-    try {
-      await fs.unlink(filePath);
-    } catch (err) {
-      console.error("Error:", err.message);
-    }
-  }
-
-  deleteFile(`./memos/${answer}.txt`);
-} else {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  const lines = [];
-
-  rl.on("line", (input) => {
-    if (input === "") return rl.close();
-    lines.push(input);
-  });
-
-  rl.on("close", async () => {
-    await fs.mkdir("memos", { recursive: true });
-    await fs.writeFile(`./memos/${lines[0]}.txt`, lines.join("\n"));
-  });
 }
+
+class File {
+  constructor() {
+    this.directory = "memos";
+  }
+
+  async _save(filename, lines) {
+    await fs.mkdir(this.directory, { recursive: true });
+    await fs.writeFile(`./${this.directory}/${filename}.txt`, lines.join("\n"));
+  }
+
+  async _handleContent(callback) {
+    try {
+      return await callback();
+    } catch (err) {
+      console.error("Error:", err.message);
+      return null;
+    }
+  }
+
+  async _readFirstLine(directoryPath, callback) {
+    try {
+      await fs.mkdir(this.directory, { recursive: true });
+      const files = await fs.readdir(directoryPath);
+      for (const file of files) {
+        const filePath = path.join(directoryPath, file);
+        const content = await fs.readFile(filePath, "utf8");
+        const firstLine = content.split("\n")[0];
+        await callback(firstLine);
+      }
+    } catch (err) {
+      console.error("Error:", err);
+    }
+  }
+}
+
+class Memo extends File {
+  constructor() {
+    super();
+    this.memoapp = new MemoApp();
+  }
+
+  async add() {
+    const lines = await this.memoapp.input();
+    const filename = lines[0];
+    await this._save(filename, lines);
+  }
+
+  async delete() {
+    const lines = await this.#getFirstLines("./memos");
+
+    const memoapp = new MemoApp();
+    const answer = await memoapp.choose(
+      lines,
+      "Choose a note you want to delete:",
+    );
+    await this._handleContent(() => fs.unlink(`./memos/${answer}.txt`));
+  }
+  async print() {
+    await this._readFirstLine("./memos", (line) => console.log(line));
+  }
+  async printAll() {
+    const lines = await this.#getFirstLines("./memos");
+
+    const memoapp = new MemoApp();
+    const answer = await memoapp.choose(
+      lines,
+      "Choose a note you want to see:",
+    );
+
+    const content = await this._handleContent(() =>
+      fs.readFile(`./memos/${answer}.txt`, "utf8"),
+    );
+
+    console.log(content);
+  }
+
+  async #getFirstLines(directoryPath) {
+    const lines = [];
+    await this._readFirstLine(directoryPath, (line) => lines.push(line));
+    if (lines.length === 0) {
+      process.exit(0);
+    }
+    return lines;
+  }
+}
+
+const memoapp = new MemoApp(option);
+memoapp.output();
